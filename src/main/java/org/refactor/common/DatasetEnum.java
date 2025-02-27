bbdbedb8ad85f4aff438c9eeb2318f750fe6152a
cc_du <cc_du@outlook.com>
2025-02-28 04:33:31 +0800

    Fix wrong threshold

--- src/main/java/org/refactor/common/DatasetEnum.java
+++ src/main/java/org/refactor/common/DatasetEnum.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 
 public enum DatasetEnum {
 
-    ANT_5 ("ant-1.5",  DatasetConst.ROOT + "ant-1.5.0/ant-1.5/org/", new Threshold(8, 7, 8)),
-    ANT_6 ("ant-1.6",  DatasetConst.ROOT + "ant-1.6.0/ant-1.6/org/", new Threshold(8, 7, 7)),
-    ANT_7 ("ant-1.7",  DatasetConst.ROOT + "ant-1.7.0/org/", new Threshold(8, 7, 7)),
-    JEDIT_0 ("jEdit-4.0",  DatasetConst.ROOT + "jEdit-4.0/jedit40install/jedit/org/", new Threshold(11, 8, 8)),
-    JEDIT_1 ("jEdit-4.1",  DatasetConst.ROOT + "jEdit-4.0/jedit40install/jedit/org/", new Threshold(10, 10, 10)),
-    JEDIT_2 ("jEdit-4.2",  DatasetConst.ROOT + "jEdit-4.0/jedit40install/jedit/org/", new Threshold(10, 11, 11)),
-    SYNAPSE_1 ("synapse-1.1",  DatasetConst.ROOT + "apache-synapse-1.1/synapse-core-1.1/org/", new Threshold(7, 11, 11)),
+    ANT_5 ("ant-1.5",  DatasetConst.ROOT + "ant-1.5.0/ant-1.5/org/", new Threshold(8, 7, 40)),
+    ANT_6 ("ant-1.6",  DatasetConst.ROOT + "ant-1.6.0/ant-1.6/org/", new Threshold(8, 7, 31)),
+    ANT_7 ("ant-1.7",  DatasetConst.ROOT + "ant-1.7.0/org/", new Threshold(8, 7, 32)),
+    JEDIT_0 ("jEdit-4.0",  DatasetConst.ROOT + "jEdit-4.0/jedit40install/jedit/org/", new Threshold(11, 8, 32)),
+    JEDIT_1 ("jEdit-4.1",  DatasetConst.ROOT + "jEdit-4.0/jedit40install/jedit/org/", new Threshold(10, 10, 31)),
+    JEDIT_2 ("jEdit-4.2",  DatasetConst.ROOT + "jEdit-4.0/jedit40install/jedit/org/", new Threshold(10, 11, 37)),
 
     TEST ("test",  "target/test-classes/pack", new Threshold(0, 0, 0));
 
